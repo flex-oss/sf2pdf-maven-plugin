2aef965686359ae5af9c195fe626e833fd339a5f
Michael Petritsch <michael.petritsch@gmail.com>
2014-10-26 19:31:48 +0100

    added license headers to files

--- src/main/java/org/cdlflex/sf2pdf/writers/itextpdf/PDFReportDocument.java
+++ src/main/java/org/cdlflex/sf2pdf/writers/itextpdf/PDFReportDocument.java
@@ -1,3 +1,16 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.cdlflex.sf2pdf.writers.itextpdf;
 
 import java.io.ByteArrayOutputStream;
@@ -81,8 +94,8 @@ public class PDFReportDocument extends Document {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             readImages();
             writer = PdfWriter.getInstance(this, baos);
-            bfHelv = BaseFont.createFont(BaseFont.HELVETICA, "Cp1252", false);
-            bfHelvBold = BaseFont.createFont(BaseFont.HELVETICA_BOLD, "Cp1252", false);
+            bfHelv = BaseFont.createFont(BaseFont.HELVETICA, BaseFont.CP1252, false);
+            bfHelvBold = BaseFont.createFont(BaseFont.HELVETICA_BOLD, BaseFont.CP1252, false);
 
             open();
             drawTitlePage();
